59191c9f7d689e31553825dbbf82b344938eb664
mengx <981371067@qq.com>
2018-10-25 18:19:50 +0800

    First submission

--- src/main/java/xin/taowangtu/hadoop/tools/HDFSDemo.java
+++ src/main/java/xin/taowangtu/hadoop/tools/HDFSDemo.java
@@ -12,10 +12,19 @@ public class HDFSDemo {
     private final static Logger logger = LoggerFactory.getLogger(HDFSDemo.class);
 
     private FileSystem getHadoopFileSystem() {
+        String hadoopConfDir = System.getenv("HADOOP_CONF_DIR");
+
         FileSystem fs = null;
         Configuration conf = new Configuration();
+        String coreSiteXml = hadoopConfDir + "\\core-site.xml";
+        String hdfsSiteXml = hadoopConfDir + "\\hdfs-site.xml";
+        System.out.println(coreSiteXml);
+        System.out.println(hdfsSiteXml);
+        conf.addResource(new Path(coreSiteXml));
+        conf.addResource(new Path(hdfsSiteXml));
         conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
 //        conf.set("fs.defaultFS","hdfs://192.168.217.132:9000");
+        System.out.println(conf.get("fs.defaultFS"));
         try {
             fs = FileSystem.get(conf);
         } catch (IOException e) {
@@ -51,6 +60,7 @@ public class HDFSDemo {
         try {
             fs.copyFromLocalFile(file, path);
             logger.info(f + " 文件上传成功");
+            System.out.println("文件上传成功");
         } catch (IOException e) {
             e.printStackTrace();
             logger.error("", e);
@@ -64,11 +74,33 @@ public class HDFSDemo {
         }
     }
 
+    private boolean dropHdfsPath(FileSystem fs, String p) {
+        boolean b = false;
+        Path path = new Path(p);
+        try {
+            b = fs.deleteOnExit(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error("", e);
+        } finally {
+            try {
+                fs.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.error("", e);
+            }
+        }
+        return b;
+    }
+
+
     public static void main(String[] args) {
         HDFSDemo hd = new HDFSDemo();
         FileSystem fs = hd.getHadoopFileSystem();
+        System.out.println(fs);
 //        boolean status = hd.createPath(hd.getHadoopFileSystem());
-        hd.upload(fs, "D:\\hive\\out\\artifacts\\hive_jar\\hive.jar", "/user/mengxb/hadfdemo/");
+        hd.upload(fs, "e:\\Hadoop- The Definitive Guide, 4th Edition.pdf", "/user/mengxb/hadfdemo/");
+
     }
 
 
